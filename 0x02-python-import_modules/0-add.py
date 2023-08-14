c5259413a40d12b9cb3bc15ba3b90ee37dbd35fa
feliciodeangela <feliciodeangela@gmail.com>
2023-08-14 23:45:35 +0200

    up task 0 fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0.py import add
-a = 1
-b = 2
+from add_0 import add
 if __name__ == "__main__":
+    a = 1
+    b = 2
     r = add(a, b)
-print(f"{a} + (b) = {r}")
+    print(f"{a} + {b} = {r}")
