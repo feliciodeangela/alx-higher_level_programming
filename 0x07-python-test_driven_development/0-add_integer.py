daa447cddd8dcaa9514ff3aa48a5243f55da7e58
felicio <felicio@localhost.localdomain>
2023-09-13 01:38:34 +0200

    up fix task 0 test 0 #2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,9 +13,9 @@ def add_integer(a, b=98):
         TypeError: If a/bb is not an integer
     Returns:
         An integer result of the addition."""
-    if not isinstance(a, int):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
     r = int(a) + int(b)
     return (int(r))
