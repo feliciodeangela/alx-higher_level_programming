47922f1e3986f70b0e6dfebe7b2e4492e64a99c2
felicio <felicio@localhost.localdomain>
2023-09-13 01:11:56 +0200

    AT LAST! up task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Function that adds two integers
+
+    Args:
+        a (int): first number to add.
+        b (int): second numbber to add.
+
+    Raises:
+        TypeError: If a/bb is not an integer
+    Returns:
+        An integer result of the addition."""
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        raise TypeError("b must be an integer")
+    r = int(a) + int(b)
+    return (int(r))
