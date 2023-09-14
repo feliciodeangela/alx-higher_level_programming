84a74022d102c6a99110e5019abaabb2a2f3ba3a
feliciodeangela <feliciodeangela@gmail.com>
2023-09-14 12:36:25 +0000

    up task 3 test 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains the definition of the say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Definition of the say_my_name function
+
+    Args:
+        first_name (string): Represents the first name.
+        last_name (string): Represents the last name.
+
+    Raises:
+        TypeError: If first_name|last_name is not a string."""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
