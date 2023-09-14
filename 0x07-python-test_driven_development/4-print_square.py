cfc28fe9ce86e0c79946df194f4c598445459e7b
feliciodeangela <feliciodeangela@gmail.com>
2023-09-14 14:26:27 +0000

    up task 4 test 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyth3
+"""This module contains the definition of the print_square function."""
+
+
+def print_square(size):
+    """function that prints a square
+
+    Args:
+        size (int): Size of the sqare's size."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if not isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    elif size > 0:
+        for i in range(0, size):
+            for j in range(0, size):
+                print("#", end='')
+            print()
+    print()
