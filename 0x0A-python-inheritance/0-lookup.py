1c5a66abfc8dec173c0092f67bcee817e2ad3704
feliciodeangela <feliciodeangela@gmail.com>
2023-09-18 09:09:24 +0000

    up task 0 + README

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+"""This module contains the definition of the lookup function."""
+
+
+def lookup(obj):
+    """lookup function definition
+
+    Args:
+        obj (Object): Object to look methods up.
+    Returns:
+        A list containing all object's methods."""
+    return (dir(obj))
