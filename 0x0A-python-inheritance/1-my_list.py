76f276cb2ad8399b1924ee35f5836b3f70825195
feliciodeangela <feliciodeangela@gmail.com>
2023-09-18 10:27:13 +0000

    up task 1 update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,9 @@ class MyList(list):
     """MyList definition."""
 
     def print_sorted(self):
-        """Function to print a list in ascending order"""
-        print(sorted(self))
+        """Function to print a list in ascending order."""
+        return (sorted(self))
+
+    def __str__(self):
+        """str() method for MyList class."""
+        return ("{}".format(self))
