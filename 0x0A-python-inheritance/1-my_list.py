a663647e9f1f34604341945391eca2acfba27b04
feliciodeangela <feliciodeangela@gmail.com>
2023-09-18 10:07:58 +0000

    up task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the MyList class definition."""
+
+
+class MyList(list):
+    """MyList definition."""
+
+    def print_sorted(self):
+        """Function to print a list in ascending order"""
+        print(sorted(self))
