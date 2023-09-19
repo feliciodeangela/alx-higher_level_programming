425226eb512f7808309b2a5e4e5d11562765709f
Felício <89345427+feliciodeangela@users.noreply.github.com>
2023-09-19 11:03:11 +0200

    up task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the definition of the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class defitnition."""
+    
+    def area(self):
+        """Calculates the geometrical area."""
+        raise Exception("area() is not implemented")
