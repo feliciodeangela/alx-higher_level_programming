44f7603bf22311e3dad3af49724af8bc6be7caeb
Felício <89345427+feliciodeangela@users.noreply.github.com>
2023-09-19 11:58:21 +0200

    up task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains the definition of the BaseGeometry class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle class defitnition."""
+
+    def __init__(self, width, height):
+        """Rectangle class contructor.
+
+        Args:
+            width (int): Width of the rectangle form.
+            height (int): Height of the rectangle form."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
