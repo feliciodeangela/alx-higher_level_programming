73374bf2bb3a68cc0e81c0c1069c51fe9cebdd06
feliciodeangela <feliciodeangela@gmail.com>
2023-09-20 13:04:50 +0000

    up task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains the definition of the read_file function."""
+
+
+def read_file(filename=""):
+    """Function to read a file.
+
+    Args:
+        filename (str): Name/Directory of the file to read."""
+    with open(filename, encoding="utf-8") as fl:
+        print(fl.read())
