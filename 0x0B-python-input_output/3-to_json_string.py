b4bf42204dbbcd808fa59ef164314672a4787410
feliciodeangela <feliciodeangela@gmail.com>
2023-09-20 14:00:49 +0000

    up task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains the definition of the to_json_string."""
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representationof an string object.
+
+    Args:"""
