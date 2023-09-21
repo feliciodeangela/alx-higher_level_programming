57a1f9f8e5a0f0a0ca30b39666193c64f12faa0f
felicio <felicio@localhost.localdomain>
2023-09-21 12:23:25 +0200

    up task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a script that reads input and adds them to a file.."""
+import sys
+#import json
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+ls = []
+for arg in range(1, len(sys.argv)):
+    ls.append(sys.argv[arg])
+save(ls, 'add_item.json')
+load('add_item.json')
