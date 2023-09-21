d2507860680da040b2fd73eb1dac4674ae8d3423
felicio <felicio@localhost.localdomain>
2023-09-21 12:33:55 +0200

    up fix documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,8 @@
 import sys
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
+
+
 ls = load('add_item.json')
 for arg in range(1, len(sys.argv)):
     ls.append(sys.argv[arg])
