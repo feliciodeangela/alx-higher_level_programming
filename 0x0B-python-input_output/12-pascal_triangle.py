6246de41fcb171cef8ebc38ba396aa462d8737f2
felicio <felicio@localhost.localdomain>
2023-09-22 00:31:42 +0200

    up task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains the definition of the pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """Creates numerical representation of Pascal's triangle
+
+    Args:
+        n (int): Size (base)
+    Returns:
+        List of lists of integers representing the triangle of n"""
+    ls = [[1]]
+    if n <= 0:
+        return (ls)
+    for i in range(1, n):
+        r = [1]
+        for j in range(1, i):
+            r.append(ls[i-1][j-1] + ls[i-1][j])
+        r.append(1)
+        ls.append(r)
+    return (ls)
