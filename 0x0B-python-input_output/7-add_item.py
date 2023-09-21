758cb5a950770ac224322256303b95932a7b52fc
felicio <felicio@localhost.localdomain>
2023-09-21 12:31:25 +0200

    up task 7 fix: load to list first, append later.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,8 +3,7 @@
 import sys
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
-ls = []
+ls = load('add_item.json')
 for arg in range(1, len(sys.argv)):
     ls.append(sys.argv[arg])
 save(ls, 'add_item.json')
-load('add_item.json')
