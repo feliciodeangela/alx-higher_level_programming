c9363d5ad65891ac007af4506d1f4aa93e32365a
felicio <felicio@localhost.localdomain>
2023-10-01 12:31:30 +0200

    up main 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,6 +21,23 @@ class Square(Rectangle):
         return ("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.size))
 
+    def update(self, *args, **kwargs):
+        """Square's attributes updater"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for keys, val in kwargs.items():
+                setattr(self, keys, val)
+
     @property
     def size(self):
         """Sqaure's size getter"""
