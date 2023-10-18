7b3001892d7fc7866ddd421159a5b53ccc59e659
felicio <feliciodeangela@gmail.com>
2023-10-18 14:33:07 +0200

    up task 15 fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Display number of records with the same score
-SELECT score, COUNT(score) as number FROM second_table ORDER BY number DESC GROUP BY score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY number DESC;
