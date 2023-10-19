1468ea723c08a33fdcdab89c49710cb790b6c9d1
felicio <feliciodeangela@gmail.com>
2023-10-19 15:14:00 +0200

    up task 9 fix #3

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Select from multiple tables
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.states_id = states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id = cities.states_id ORDER BY cities.id;
