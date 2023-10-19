905f3669cfc6d164f9580d32f1fb3abbbb46c563
felicio <feliciodeangela@gmail.com>
2023-10-19 22:42:20 +0200

    up yask 10 fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- List all shows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genres_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title AND tv_show_genres.genre_id;
