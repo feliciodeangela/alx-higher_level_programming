b9d2f6bdef33abf74af29a2d1580d3e837090045
felicio <feliciodeangela@gmail.com>
2023-10-21 09:47:01 +0200

    up task 12 fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- List all shows without genre
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows WHERE tv_show_genres.genre_id IS NULL LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows WHERE tv_show_genres.genre_id IS NULL RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
