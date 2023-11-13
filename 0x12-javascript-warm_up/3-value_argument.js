8b354723e59db5465a607dceaa31118d62242f45
felicio <feliciodeangela@gmail.com>
2023-11-13 23:15:40 +0200

    up task 3 fix length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length === 2) {
-  console.log('No Arguments');
-} else {
+if (argv[2]) {
   console.log(argv[2]);
+} else {
+  console.log('No Arguments');
 }
