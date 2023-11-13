65e1f12ef23bf44ec3cfe878dd5361855846e2e5
felicio <feliciodeangela@gmail.com>
2023-11-13 23:20:24 +0200

    up task 2 & 3 process.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length > 2) {
+if (processargv.length > 2) {
   console.log('Arguments found');
-} else if (argv.length === 2) {
+} else if (process.argv.length === 2) {
   console.log('Argument found');
 } else {
   console.log('No argument');
