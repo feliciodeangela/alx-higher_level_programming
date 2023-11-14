c7267ae872b07a6e0a2cb49024dbe44cb0fcd59d
feliciodeangela <feliciodeangela@gmail.com>
2023-11-14 06:56:47 -0800

    up task 11 semi

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
-  let newArr = process.argv.slice(2);
+  const newArr = process.argv.slice(2);
   newArr.sort();
-  console.log(newArr[1]);
+  console.log(Number(newArr[1]));
 } else {
   console.log(0);
 }
