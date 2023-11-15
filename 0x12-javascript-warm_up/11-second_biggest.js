af8e9527e020b2479d39dda580b184c98fac3565
felicio <feliciodeangela@gmail.com>
2023-11-15 22:22:31 +0200

    up task 11 biggest instead of smallest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
-  const newArr = process.argv.slice(2);
+  const newArr = process.argv.map(Number).slice(2);
   newArr.sort();
-  console.log(Number(newArr[newArr.length - 2]));
+  console.log(newArr[newArr.length - 2]);
 } else {
   console.log(process.argv.length);
 }
