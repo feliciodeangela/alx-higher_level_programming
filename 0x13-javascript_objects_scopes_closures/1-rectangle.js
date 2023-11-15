9a367709f0210c0f22100784a169b2a2c12813e2
felicio <feliciodeangela@gmail.com>
2023-11-15 04:28:33 +0200

    correct task 1 output

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-module.export = class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    this.height = h;
-    this.width = w;
+    if (w && h) {
+      this.height = h;
+      this.width = w;
+    }
   }
 };
