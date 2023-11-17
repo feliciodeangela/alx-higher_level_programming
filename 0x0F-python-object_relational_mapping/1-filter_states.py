58767282a0e7864c4e0b9c7059accf40e3e3604a
feliciodeangela <feliciodeangela@gmail.com>
2023-11-17 17:33:51 +0200

    up task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Filtered states selection module"""
+import sys
+import MySQLdb
+
+
+def dbConnect():
+    """Filtered states selection function"""
+    link = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+    search = "N%"
+    exe = link.cursor()
+    exe.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id",(search,))
+    link.commit()
+    result = exe.fetchall()
+    for res in result:
+        print(res)
+    exe.close()
+    link.close()
+
+
+if __name__ == "__main__":
+    dbConnect()
