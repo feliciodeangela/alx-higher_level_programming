958698542529745f6391f28e7d477af26af5c6e8
feliciodeangela <feliciodeangela@gmail.com>
2023-12-22 23:09:33 +0200

    up task 2 try#4

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     data = data.encode("utf8")
     req = request.Request(argv[1], data)
     with request.urlopen(req) as resp:
-        print("Your email is: {}".format(resp.read().decode("utf8")))
+        print("{}".format(resp.read().decode("utf8")))
