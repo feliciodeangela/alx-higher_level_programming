aecd702d9628fc3968d6c22dbdc6258951623d03
feliciodeangela <feliciodeangela@gmail.com>
2023-12-22 13:10:41 +0200

    up task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for url fetch"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        res = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode(encoding='utf-8')))
