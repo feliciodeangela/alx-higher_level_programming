e61f2a77d7432d8526a4e4bb89e12c808da88959
feliciodeangela <feliciodeangela@gmail.com>
2023-12-22 22:58:24 +0200

    up task 2 try

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Send a POST request"""
+from urllib import request, parse
+from sys import argv
+
+
+if __name__== "__main__":
+    email = argv[2]
+    data = parse.urlencode({"email": email})
+    req = request.Request(argv[1], data.encode("utf8"))
+    with urlopen(req) as resp:
+        print("Your email is: {}".format(resp.info()["email"]))
