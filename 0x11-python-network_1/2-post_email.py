1556365ae3e1d2da010ceb7c28d95556877d6978
feliciodeangela <feliciodeangela@gmail.com>
2023-12-22 22:59:07 +0200

    up task 2 try

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,7 @@ from urllib import request, parse
 from sys import argv
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     email = argv[2]
     data = parse.urlencode({"email": email})
     req = request.Request(argv[1], data.encode("utf8"))
