922bb6c3b205ea4d6808bf68b073cb71de7ef20b
feliciodeangela <feliciodeangela@gmail.com>
2023-12-30 13:35:20 +0200

    up task 3 #2

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,6 +7,6 @@ from sys import argv
 if __name__ == "__main__":
     try:
         with request.urlopen(argv[1]) as resp:
-            print("{}".format(resp))
+            print("{}".format(resp.read().decode("utf8")))
     except error.HTTPError as err:
         print("Error code: {}".format(err.code))
