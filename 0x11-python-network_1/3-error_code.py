ca29e7f829ad5ba425379ce4658297b1f9198bbb
feliciodeangela <feliciodeangela@gmail.com>
2023-12-30 13:31:47 +0200

    up task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Show body of response and take care of error codes"""
+from urllib import error, request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as resp:
+            print("{}".format(resp.read()))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
