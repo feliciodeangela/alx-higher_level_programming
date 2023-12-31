1473435c2942cbd6acd27fadb8d366ea7b46799a
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 11:59:25 +0200

    up task 7 #2

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     try:
         resp = requests.get(argv[1])
+        resp.raise_for_status()
         print(resp.content.decode("utf8"))
     except requests.exceptions.HTTPError as err:
         print("Error code: {}".format(err))
