5119220f339068df2445af79decbcc1bc962fb12
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 23:02:31 +0200

    up task 10 #2

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,6 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://api.github.com/{}".format(argv[1])
-    hdr = {"Authorization": " token {}".format(argv[2])}
+    hdr = {"Authorization": "token {}".format(argv[2])}
     resp = requests.get(url, headers=hdr)
     print(resp)
