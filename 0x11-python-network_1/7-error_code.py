aa3b05109513792e9dd627e932844134210a7786
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 11:51:33 +0200

    up task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Send request and show error if it hapens"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        resp = requests.get(argv[1])
+        print(resp.content.decode("utf8"))
+    except requests.exceptions.HTTPError as err:
+        print("Error code: {}".format(err.code))
