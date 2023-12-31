300051bf9344103b8b7c89445d21f93c158c8db6
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 23:13:56 +0200

    up task 10 #5

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,4 +8,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/users/{}".format(argv[1])
     hdr = {"Authorization": "token {}".format(argv[2])}
     resp = requests.get(url, headers=hdr)
-    print(resp.json())
+    if resp.json().get('id'):
+        print(resp.json().get('id'))
+    else:
+        print("None")
