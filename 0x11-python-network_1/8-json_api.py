bd92294fa938dc7c18c5d7599db67732b0fbba9a
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 17:45:25 +0200

    up task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""""Send a text in a POST"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    let = ""
+    if argv[1]:
+        let = argv[1]
+    try:
+        res = requests.post("http://0.0.0.0:5000/search_user", data={"q": le})
+        result = res.json()
+        if not result:
+            print("No result")
+    except requests.JSONDecodeError as err:
+        print("Not a valid JSON")
+    else:
+        print("[{}] {}".format(result.id, result.name))
