5700e2e86e3b292c50f8541042d84450407018da
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 22:57:33 +0200

    up task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Authenticate and display my github id"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/{}".format(argv[1])
+    hdr = {"Authorization": " token {}".format(argv[2])}
+    resp = requests.get(url, headers=hdr)
+    print(resp.get('id'))
