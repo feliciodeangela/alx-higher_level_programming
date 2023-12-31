d209ac463905289c8e9315e9eef6fee1c712686c
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 12:27:18 +0200

    up task 7 #7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ from sys import argv
 if __name__ == "__main__":
     resp = requests.get(argv[1])
     if resp.status_code >= 400:
-        print("Error code: ".format(resp.status_code))
+        print("Error code: {}".format(resp.status_code))
     else:
         print(resp.content.decode("utf8"))
