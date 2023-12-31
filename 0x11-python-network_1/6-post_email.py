865719d123692cee8a77372d08bafc05ef2f1cae
feliciodeangela <feliciodeangela@gmail.com>
2023-12-31 11:27:48 +0200

    up task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""POST a request and display part of the request's parameters in response"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    resp = requests.post(argv[1], data={'email': argv[2]})
+    print("Your email is: {}".format(resp.content.decode("utf8")))
